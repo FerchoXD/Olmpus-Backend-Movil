7f83ee66da198febe49b3dffe856a89fd8960f8e
Max-500 <213497@ids.upchiapas.edu.mx>
2024-04-04 14:20:08 -0600

    Se obtiene la preview

--- src/BookManagment/Infraestructure/Repositories/MySQL/BookMySQLRepository.ts
+++ src/BookManagment/Infraestructure/Repositories/MySQL/BookMySQLRepository.ts
@@ -1,6 +1,8 @@
+import { where } from "sequelize";
 import sequelize from "../../../../Database/Config/MySQL/Database";
 import { Book } from "../../../Domain/Entities/Book";
 import { IBook } from "../../../Domain/Ports/IBook";
+import { BookModel } from "../../Database/Models/MySQL/BookModel";
 
 export class BookMySQLRepository implements IBook {
     async recommendBook(userUUID: string): Promise<Book[]|any> {
@@ -16,7 +18,38 @@ export class BookMySQLRepository implements IBook {
     }
 
     async getPreviewBook(uuid: string): Promise<Book|any> {
-        throw new Error("Method not implemented.");
+        try {
+            const book = await BookModel.findOne({ where: { uuid: uuid }, attributes: { exclude: ['content', 'createdAt', 'updatedAt', 'gender'] } });
+            if(!book){
+                return {
+                    "status": 404,
+                    "errors": [
+                        {
+                            "title": "Book Not Found",
+                            "detail": `The book with the requested uuid ${uuid} was not found in the database.`
+                        }
+                    ]
+                }
+            }
+
+            return {
+                "status": 200,
+                "uuid": book.dataValues.uuid,
+                "type": "books",
+                "attributes": {
+                    "title": book.dataValues.title,
+                    "description": book.dataValues.description,
+                    "image": book.dataValues.image,
+                    "author": book.dataValues.author,
+                }
+            }
+
+        } catch (error) {
+            return {
+                "status":500,
+                "error": error
+            }
+        }
     }
 
     async serchBook(name: string): Promise<Book[]|any> {
