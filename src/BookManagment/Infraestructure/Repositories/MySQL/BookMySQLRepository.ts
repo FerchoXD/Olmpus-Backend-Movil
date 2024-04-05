96be282e2b962b62a561dbaafe532f0a48c75684
Max-500 <213497@ids.upchiapas.edu.mx>
2024-04-05 10:04:37 -0600

    Modificacion de ruta y getAll finish

--- src/BookManagment/Infraestructure/Repositories/MySQL/BookMySQLRepository.ts
+++ src/BookManagment/Infraestructure/Repositories/MySQL/BookMySQLRepository.ts
@@ -1,3 +1,4 @@
+import Sequelize from "sequelize";
 import sequelize from "../../../../Database/Config/MySQL/Database";
 import { Book } from "../../../Domain/Entities/Book";
 import { IBook } from "../../../Domain/Ports/IBook";
@@ -8,7 +9,34 @@ export class BookMySQLRepository implements IBook {
     }
     
     async getBooks(): Promise<Book[]|any> {
-        throw new Error("Method not implemented.");
+        try {
+            const query = `SELECT uuid, image, author, description, title, gender FROM books ORDER BY RAND() LIMIT 50;`;
+            const data = await sequelize.query(query, { type: Sequelize.QueryTypes.SELECT } );
+
+            if(data.length === 0){
+                return {
+                    "status": 200,
+                    "data": []
+                }
+            }
+
+            const books:Book[] = [];
+            data.forEach((book:any) => {
+                const bookEntity = new Book(book.title, book.description, book.author, book.image, book.content, book.gender);
+                bookEntity.uuid = book.uuid
+                books.push(bookEntity)
+            });
+            return {
+                "status": 200,
+                "type": "books",
+                "data": books
+            }
+        } catch (error) {
+            return {
+                "status":500,
+                "error": error
+            }
+        }
     }
 
     async readBook(uuid: string, userUUID:string): Promise<Book|any> {
