4ca848f4eac5289217f54e55885c94d450d4b0ae
Fernando Daniel Pérez Pérez <213524@ids.upchiapas.edu.mx>
2024-04-09 03:10:25 -0600

    implementaciones de 4 enpoints

--- src/BookManagment/Infraestructure/Repositories/MySQL/BookMySQLRepository.ts
+++ src/BookManagment/Infraestructure/Repositories/MySQL/BookMySQLRepository.ts
@@ -1,6 +1,8 @@
 import Sequelize from "sequelize";
 import sequelize from "../../../../Database/Config/MySQL/Database";
 import { Book } from "../../../Domain/Entities/Book";
+import { History } from "../../../Domain/Entities/History";
+import { HistoryModel } from "../../Database/Models/MySQL/HistoryModel";
 import { IBook } from "../../../Domain/Ports/IBook";
 
 export class BookMySQLRepository implements IBook {
@@ -39,10 +41,49 @@ export class BookMySQLRepository implements IBook {
         }
     }
 
-    async readBook(uuid: string, userUUID:string): Promise<Book|any> {
-        throw new Error("Method not implemented.");
-    }
+    async readBook(uuid: string, userUUID: string): Promise<Book | any> {
+        try {
+            const query = `SELECT uuid, image, author, description, title, content, gender FROM books WHERE uuid = '${uuid}'`;
+            const [bookData]: any[] = await sequelize.query(query, { type: Sequelize.QueryTypes.SELECT });
 
+            if (!bookData) {
+                return {
+                    "status": 404,
+                    "error": "Book not found"
+                };
+            }
+
+            if (!('title' in bookData && 'description' in bookData && 'author' in bookData && 'image' in bookData)) {
+                return {
+                    "status": 500,
+                    "error": "Invalid book data"
+                };
+            }
+
+            const bookEntity = new Book(bookData.title, bookData.description, bookData.author, bookData.image, bookData.content, bookData.gender);
+            bookEntity.uuid = bookData.uuid;
+
+            const history = new History(userUUID, uuid, new Date());
+            const savedHistory = await HistoryModel.create({
+                uuid: history.uuid,
+                bookUUID: bookEntity.uuid,
+                readingDate: history.readingDate.toISOString(),
+                userUUID: userUUID
+            });
+
+            return {
+                "status": 200,
+                "type": "book",
+                "data": bookEntity
+            };
+        } catch (error) {
+            return {
+                "status": 500,
+                "error": `Error fetching book: ${error}`
+            }
+        }
+    }
+    
     async getPreviewBook(uuid: string): Promise<Book|any> {
         throw new Error("Method not implemented.");
     }
--- src/BookManagment/Infraestructure/Repositories/MySQL/HistoryMySQLRepository.ts
+++ src/BookManagment/Infraestructure/Repositories/MySQL/HistoryMySQLRepository.ts
@@ -1,8 +1,47 @@
+import Sequelize from "sequelize";
+import sequelize from "../../../../Database/Config/MySQL/Database";
 import { Book } from "../../../Domain/Entities/Book";
 import { IHistory } from "../../../Domain/Ports/IHistory";
 
 export class HistoryMySQLRepository implements IHistory {
-    async getBooksRead(userUUID: string): Promise<Book[]|any> {
-        throw new Error("Method not implemented.");
+    async getBooksRead(userUUID: string): Promise<Book[] | any> {
+        try {
+            // Consulta SQL para obtener los libros leídos por el usuario
+            const query = `
+                SELECT b.uuid, b.title, b.description, b.author, b.image, b.content, b.gender
+                FROM books AS b
+                INNER JOIN histories AS h ON b.uuid = h.bookUUID
+                WHERE h.userUUID = :userUUID;
+            `;
+            const data = await sequelize.query(query, {
+                type: Sequelize.QueryTypes.SELECT,
+                replacements: { userUUID }
+            });
+
+            if (data.length === 0) {
+                return {
+                    "status": 200,
+                    "data": []
+                };
+            }
+
+            // Mapear los datos obtenidos a objetos Book
+            const booksRead: Book[] = data.map((book: any) => {
+                const bookEntity = new Book(book.title, book.description, book.author, book.image, book.content, book.gender);
+                bookEntity.uuid = book.uuid;
+                return bookEntity;
+            });
+
+            return {
+                "status": 200,
+                "type": "books",
+                "data": booksRead
+            };
+        } catch (error) {
+            return {
+                "status": 500,
+                "error": error
+            };
+        }
     }
-}
+    }
